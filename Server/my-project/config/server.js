f4731e75dd1e9de60407d5e2a25e144636f02443
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-10-25 14:23:57 +0100

    server update

---
+++ Server/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5b310e674435c5888678b68dbd4685c6'),
+    },
+  },
+});
